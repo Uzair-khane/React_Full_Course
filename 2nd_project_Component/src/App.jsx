e6a62bbc9a3cc82015b06ad07fb4d841ad8f6bef
Uzair-khane <uzairkhanar123@gmail.com>
2026-04-15 16:54:04 +0500

    learn about component in react

---
+++ 2nd_project_Component/src/App.jsx
@@ -0,0 +1,14 @@
+import Hello from "./Hello"
+
+function App() {
+const appName = "App Component"
+
+  return (
+    <>
+     <h1>Hello {appName}</h1>
+     <Hello/>
+    </>
+  )
+}
+
+export default App
---
+++ 2nd_project_Component/src/Hello.jsx
@@ -0,0 +1,7 @@
+function Hello(){
+    const hellocom ="This is Hello Component"
+return(
+    <h1>Hello {hellocom}</h1>
+)
+}
+export default Hello;
