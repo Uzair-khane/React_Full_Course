2e8dc6808e9fbaa6dbe35c6ccf17f4f384e44960
Uzair-khane <uzairkhanar123@gmail.com>
2026-04-24 15:05:47 +0500

    learn event

--- 3rd_Project_Function_Event/src/Greet.jsx
+++ 3rd_Project_Function_Event/src/Greet.jsx
@@ -2,11 +2,28 @@ function Greet(){
 // simple funtion 
 function helloUser(){
     return "heloo user"
+
+}
+const getName = (name)=>{
+ return "welcome"+" "+name
+} 
+function showUser(){
+alert(`welcome Uzair kahn`)
+console.log("hiiii")
+console.error("errorro");
+
+}
+function handleInput(event){
+    console.log(event.target.value)
+    console.log("hi")
 }
     return(
          <>
       <h1>Hello this is Greet Component</h1>
       <p>{helloUser()}</p>
+      <h1>{getName("khan")}</h1>
+      <button onClick={showUser}>Show</button>
+      <input type="text" onChange={handleInput} name="" id="" />
     </>
     )
 }
