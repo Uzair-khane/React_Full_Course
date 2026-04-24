7445badf8fc4f1c925ef6e2e4abb165c8490a07d
Uzair-khane <uzairkhanar123@gmail.com>
2026-04-24 13:08:35 +0500

    learn about function in react

---
+++ 3rd_Project_Function_Event/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
+import Greet from "./Greet"
+function App() {
+
+  return (
+    <>
+      <h1>Hello this is main component</h1>
+      <Greet/>
+    </>
+  )
+}
+
+export default App
---
+++ 3rd_Project_Function_Event/src/Greet.jsx
@@ -0,0 +1,13 @@
+function Greet(){
+// simple funtion 
+function helloUser(){
+    return "heloo user"
+}
+    return(
+         <>
+      <h1>Hello this is Greet Component</h1>
+      <p>{helloUser()}</p>
+    </>
+    )
+}
+export default Greet
